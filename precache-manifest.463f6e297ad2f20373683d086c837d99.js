5f9c6f7502e10a83b8f1c51b02906473cbcf315b
Ha Vuong <havuong@triip.me>
2020-04-06 15:20:59 +0700

    Updates

---
+++ precache-manifest.463f6e297ad2f20373683d086c837d99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f7e28936f55e89c71ffa409019f056",
+    "url": "/budget-app-react-ts/index.html"
+  },
+  {
+    "revision": "2048f1b0866f4fe64ecd",
+    "url": "/budget-app-react-ts/static/css/main.4fce1333.chunk.css"
+  },
+  {
+    "revision": "68c7f678d8d284ce3b28",
+    "url": "/budget-app-react-ts/static/js/2.81a4ce9d.chunk.js"
+  },
+  {
+    "revision": "2048f1b0866f4fe64ecd",
+    "url": "/budget-app-react-ts/static/js/main.1e224017.chunk.js"
+  },
+  {
+    "revision": "5bdf32c5cfc01d579d9e",
+    "url": "/budget-app-react-ts/static/js/runtime-main.805c93cd.js"
+  }
+]);
